8cc67afafde03cd81839974ceb541755aa933170
GeonwooKo <kw102222@gmail.com>
2021-08-04 21:42:27 +0900

    update

--- src/main.py
+++ src/main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#!/usr/bin/env python3
 
 
 import os.path
