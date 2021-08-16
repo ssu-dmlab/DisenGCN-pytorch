e0eeacb8851f3ec8a046629abaf536805ef86193
GEONWOOKO <kw1022222@naver.com>
2021-08-16 22:41:30 +0900

    Change Evaluation Mode

--- src/models/train.py
+++ src/models/train.py
@@ -7,13 +7,14 @@ from math import isnan
 from tqdm import tqdm
 from copy import deepcopy
 from models.model import DisenGCN
-
+from models.eval import MyEvaluator
 
 
 class MyTrainer:
     def __init__(self, in_dim, device='cpu'):
         self.in_dim = in_dim
         self.device = device
+        self.evaluator = MyEvaluator(device)
 
     def train_model(self, dataset, hyperpm, verbose):
         torch.autograd.set_detect_anomaly(True)
@@ -27,8 +28,6 @@ class MyTrainer:
         #learning rate decay
         # scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer=optimizer, lr_lambda=lambda epoch: 0.95 ** epoch, last_epoch=-1, verbose=False)
 
-        model.train()
-
         pbar = tqdm(range(epochs), position=1, leave=False, desc='epoch', disable=not verbose)
         early_count, best_acc, best_model = 0, 0, None
 
@@ -38,12 +37,16 @@ class MyTrainer:
 
 
         for epoch in pbar:
+            model.train()
             optimizer.zero_grad()
-            pred_prob = model(feat, src_trg_edges)
 
-            pred_label = torch.argmax(pred_prob, dim=1)
-            trn_acc = (pred_label[trn_idx] == targ[trn_idx]).sum() / len(trn_idx)
-            val_acc = (pred_label[val_idx] == targ[val_idx]).sum() / len(val_idx)
+            pred_prob = model(feat, src_trg_edges)
+            loss = F.nll_loss(pred_prob[trn_idx], targ[trn_idx])
+            loss.backward()
+            optimizer.step()
+
+            trn_acc = self.evaluator.evaluate(model, dataset, trn_idx)
+            val_acc = self.evaluator.evaluate(model, dataset, val_idx)
 
             if val_acc > best_acc:
                 best_acc, best_model = val_acc, deepcopy(model.state_dict())
@@ -51,9 +54,7 @@ class MyTrainer:
             else:
                 early_count += 1
 
-            loss = F.nll_loss(pred_prob[trn_idx], targ[trn_idx])
-            loss.backward()
-            optimizer.step()
+
             # scheduler.step()
             if verbose:
                 pbar.write(
