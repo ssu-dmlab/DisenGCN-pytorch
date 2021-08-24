47122177680e5fe358b9be98f99e4199bbcd88f4
GeonwooKo <kw102222@gmail.com>
2021-08-24 11:55:48 +0900

    train module

--- src/models/train.py
+++ src/models/train.py
@@ -1,13 +1,11 @@
 import torch
-import torch.nn
-import numpy as np
 import torch.nn.functional as F
 
-from math import isnan
 from tqdm import tqdm
 from copy import deepcopy
 from models.model import DisenGCN
 from models.eval import MyEvaluator
+from loguru import logger
 
 
 class MyTrainer:
@@ -16,25 +14,19 @@ class MyTrainer:
         self.device = device
         self.evaluator = MyEvaluator(device)
 
-    def train_model(self, dataset, hyperpm, verbose):
-        torch.autograd.set_detect_anomaly(True)
-
-        self.hyperpm = hyperpm
+    def train_model(self, dataset, hyperpm):
         epochs = hyperpm['nepoch']
 
-        model = DisenGCN(self.in_dim, dataset.get_nclass(), self.hyperpm, verbose).to(self.device)
+        model = DisenGCN(self.in_dim, dataset.get_nclass(), hyperpm).to(self.device)
+        model.acc_list = []
         optimizer = torch.optim.Adam(model.parameters(), lr=hyperpm['lr'], weight_decay=hyperpm['reg'])
 
-        #learning rate decay
-        # scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer=optimizer, lr_lambda=lambda epoch: 0.95 ** epoch, last_epoch=-1, verbose=False)
-
-        pbar = tqdm(range(epochs), position=1, leave=False, desc='epoch', disable=not verbose)
+        pbar = tqdm(range(epochs), position=1, leave=False, desc='epoch')
         early_count, best_acc, best_model = 0, 0, None
 
         trn_idx, val_idx, tst_idx = dataset.get_idx()
         _, feat, targ = dataset.get_graph_feat_targ()
         src_trg_edges = dataset.get_src_trg_edges()
-
 
         for epoch in pbar:
             model.train()
@@ -47,6 +39,7 @@ class MyTrainer:
 
             trn_acc = self.evaluator.evaluate(model, dataset, trn_idx)
             val_acc = self.evaluator.evaluate(model, dataset, val_idx)
+            model.acc_list.append((trn_acc, val_acc))
 
             if val_acc > best_acc:
                 best_acc, best_model = val_acc, deepcopy(model.state_dict())
@@ -54,12 +47,9 @@ class MyTrainer:
             else:
                 early_count += 1
 
-
-            # scheduler.step()
-            if verbose:
-                pbar.write(
-                    f'Epoch : {epoch + 1:02}/{epochs}    loss : {loss:.4f}    trn_acc : {trn_acc:.4f} val_acc : {val_acc:.4f}')
-                pbar.update()
+            pbar.write(
+                f'Epoch : {epoch + 1:02}/{epochs}    loss : {loss:.4f}    trn_acc : {trn_acc:.4f} val_acc : {val_acc:.4f}')
+            pbar.update()
 
             if (hyperpm['early'] != None and early_count == hyperpm['early']):
                 break
