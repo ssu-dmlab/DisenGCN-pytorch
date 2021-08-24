51f7cae27e775b4940bb5b282dcfc5cff207f349
GeonwooKo <kw102222@gmail.com>
2021-08-24 11:55:05 +0900

    evaluation module

--- src/models/eval.py
+++ src/models/eval.py
@@ -1,6 +1,7 @@
 import torch
 from utils import sprs_torch_from_scipy
 
+
 class MyEvaluator:
     def __init__(self, device):
         self.device = device
@@ -12,8 +13,8 @@ class MyEvaluator:
             src_trg = dataset.get_src_trg_edges()
 
             eval_pred_prob = model(feat, src_trg)[eval_idx]
-            eval_pred_label = torch.argmax(eval_pred_prob, dim = 1)
+            eval_pred_label = torch.argmax(eval_pred_prob, dim=1)
             eval_acc = (eval_pred_label == targ[eval_idx]).sum() / len(eval_idx)
-
+            eval_acc = eval_acc.item()
 
             return eval_acc
